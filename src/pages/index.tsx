c7be44800280abbd1b068377a0409cab202f6a91
Qr <skyluckin@gmail.com>
2023-03-29 03:06:11 +0800

    fix:bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -107,7 +107,7 @@ export default function Home() {
                 answer: res.data,
               },
             ]);
-            console
+   
             setLoading(false);
           },
           (err) => {
