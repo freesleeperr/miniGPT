550d2c60e7aa23b94359e5be3af7e3c08fee67d4
Qr <skyluckin@gmail.com>
2023-04-19 01:05:51 +0800

    fix:clean

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,6 +74,7 @@ export default function Home() {
   }
   function handleLogClean() {
     localStorage.removeItem("chatlog");
+    setChatlog([]);
   }
   function handleKeySave() {
     setKey(key);
