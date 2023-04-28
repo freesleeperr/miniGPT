de21d7833e3b59aad3c9cd9eb97c3d096b29f555
Qr <skyluckin@gmail.com>
2023-04-28 20:08:21 +0800

    fix

--- src/componts/DrawerSettings.tsx
+++ src/componts/DrawerSettings.tsx
@@ -111,7 +111,7 @@ export default function DrawerSettings(props: any) {
                   <SliderMark mt={"5px"} value={0}>
                     低
                   </SliderMark>
-                  <SliderMark mt={"5px"} value={4}>
+                  <SliderMark mt={"5px"} value={4.5}>
                     默认
                   </SliderMark>
                   <SliderMark mt={"5px"} value={10}>
